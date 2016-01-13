ec94cd16f1e21e5be528bd241c90f92fb5b7e347
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-13 17:59:54 +0000

    Add create action to quotes_controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,19 @@ class QuotesController < ApplicationController
   def new
     @quote = Quote.new
   end
+  
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      flash[:success] = "New quote saved!"
+      redirect_to @quote
+    else
+      flash[:error] = "There was an error!"
+      render :new
+    end
+  end
+  
+  def quote_params
+    params.require(:quote).permit(:body, :author)
+  end
 end
